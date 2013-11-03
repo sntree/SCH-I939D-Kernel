e492118dc956e685fe1910605b8619abf4cf9ac1
sn <sntree@gmail.com>
2013-11-03 23:33:41 +0800

    remove files which can re-generated

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 2 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#2 SMP PREEMPT Tue Nov 20 15:06:44 KST 2012"
-#define LINUX_COMPILE_BY "jungkilim"
-#define LINUX_COMPILE_HOST "jungkilim-desktop"
-#define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2009q3-67) "
